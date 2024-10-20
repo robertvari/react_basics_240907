ad71bdbe8307cef7a50f7a652f3c9a2385833958
Robert Vari <mail.robertvari@gmail.com>
2024-10-20 14:59:15 +0200

    Navbar with Links

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import Footer from "./components/Footer"
 import About from "./components/About";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <LandingPage/>
-      <Footer/>
-    </div>    
+    <BrowserRouter>
+
+      <div className="App">
+        <Navbar/>
+
+
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path="/services" element={<Services/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
+
+
+        <Footer/>
+      </div>
+
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Logo from "../logo.svg"
+import {Link, useLocation} from "react-router-dom"
 
 export default function Navbar() {
+  const location = useLocation()
+
+
   return (
     <div className="navbar">
       <img src={Logo} alt="logo" />
-      <a href="/">Home</a>
-      <a href="/services">Services</a>
-      <a href="/about">About</a>
-      <a href="/contact">Contact</a>
+
+      <Link to="/" className={location.pathname === "/"? "active-page" : ""}>Home</Link>
+      <Link to="/services" className={location.pathname === "/services"? "active-page" : ""}>Services</Link>
+      <Link to="/about" className={location.pathname === "/about"? "active-page" : ""}>About</Link>
+      <Link to="/contact" className={location.pathname === "/contact"? "active-page" : ""}>Contact</Link>
+
     </div>
   )
 }
