d932a28167e82067e9e73e1c3e8d83b8cb737bb5
Robert Vari <mail.robertvari@gmail.com>
2024-10-20 16:34:44 +0200

    contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>
+    <div className='contact'>
       <h2 className='title'>Contact Us</h2>
 
       <form>
         <input type="text" placeholder='Name'/>
         <input type="text" placeholder='Phone'/>
         <input type="text" placeholder='Email'/>
+        <textarea name="" id="" placeholder='Message'></textarea>
         <button>Submit</button>
       </form>
-      
     </div>
   )
 }
